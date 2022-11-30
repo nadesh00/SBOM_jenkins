d462d340fa38cd497dda3df993d96b8b672fc913
sakthiuma <sakthiumamaheswari.com>
2022-11-30 12:34:01 -0500

    Initial commit

---
+++ src/main/java/com/example/SBOM_jenkins/SbomJenkinsApplication.java
@@ -0,0 +1,14 @@
+package com.example.SBOM_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbomJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbomJenkinsApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/example/SBOM_jenkins/SbomJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SBOM_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbomJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
